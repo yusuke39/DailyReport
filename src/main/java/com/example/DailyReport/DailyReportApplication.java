4e54e1b0ee96c4b9e29c11d29dec5e0cdcd11d4d
yusuke39 <hiranoyusuke39@gmail.com>
2020-01-21 13:51:49 +0900

    first commit

---
+++ src/main/java/com/example/DailyReport/DailyReportApplication.java
@@ -0,0 +1,13 @@
+package com.example.DailyReport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DailyReport/DailyReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DailyReport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
